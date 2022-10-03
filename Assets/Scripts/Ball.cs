3975f5bdb21a88a88caddff40b2c979be0f8c73d
d9nich <54186666+d9nchik@users.noreply.github.com>
2022-10-03 22:50:54 +0300

    add basic physic to player

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] KeyCode _keyUP;
+    [SerializeField] KeyCode _keyDown;
+    [SerializeField] KeyCode _keyLeft;
+    [SerializeField] KeyCode _keyRight;
+    [SerializeField] KeyCode _keyJump;
+    [SerializeField] Vector3 _moveDirect;
+    [SerializeField] Vector3 _moveRight;
+    [SerializeField] Vector3 _moveUp;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(_keyDown))
+        {
+            GetComponent<Rigidbody>().velocity += _moveDirect;
+        }
+        if (Input.GetKey(_keyUP))
+        {
+            GetComponent<Rigidbody>().velocity -= _moveDirect;
+        }
+        if (Input.GetKey(_keyRight))
+        {
+            GetComponent<Rigidbody>().velocity += _moveRight;
+        }
+        if (Input.GetKey(_keyLeft))
+        {
+            GetComponent<Rigidbody>().velocity -= _moveRight;
+        }
+        if (Input.GetKey(_keyJump))
+        {
+            GetComponent<Rigidbody>().velocity += _moveUp;
+        }
+    }
+}
